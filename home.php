6d2932b954ad41e2784cd04743e13fea49ff0d83
StassyB <100481391+StassyB@users.noreply.github.com>
2024-02-11 17:45:04 +0300

    Added Thika

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stajel Beauty- Home</title>
+    <title>Stajel Beauty - Thika</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
